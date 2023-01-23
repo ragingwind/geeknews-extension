27f7657472a83f83113a9ecff2a1c6381bb5e471
Jimmy Moon <ragingwind@gmail.com>
2023-01-23 18:41:28 +0900

    chore: restructure project

--- scripts/build.js
+++ scripts/build.js
@@ -1,34 +1,63 @@
 import { exec as execAsync } from 'child_process';
-import util from 'util';
+import { promisify } from 'util';
+import { Manifest } from './manifest.js';
 
-const exec = util.promisify(execAsync);
+const exec = promisify(execAsync);
 
 const platform = process.argv[2] ?? 'all';
 
 async function clean() {
   console.log('Clean...');
+
   await exec('rm -rf ./dist');
+}
+
+async function build() {
+  console.log('Build...');
+
+  await exec(`mkdir -p ./dist/extension`);
+  await exec(`cp -r ./src/* ./dist/extension`);
 }
 
 async function buildChrome() {
   console.log('Build extension for Chrome...');
-  await exec(`mkdir -p ./dist/chrome`);
-  await exec(`cp -r ./apps/chrome/* ./dist/chrome`);
-  await exec(`cp -r ./extension/* ./dist/chrome/src`);
+
+  await exec(`cp -r ./dist/extension ./dist/chrome/`);
+}
+
+async function buildFirefox() {
+  console.log('Build extension for Firefox...');
+
+  try {
+    await exec(`cp -r ./dist/extension ./dist/firefox/`);
+
+    const manifest = new Manifest('./dist/firefox/manifest.json');
+    await manifest.read({
+      background: {
+        scripts: ['./background.js']
+      }
+    });
+    await manifest.write();
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 async function buildSafari() {
   console.log('Build extension for Safari...');
-  await exec(`xcrun safari-web-extension-converter --macos-only --app-name "geeknews-web-clipper" --project-location "./" --bundle-identifier "me.ragingwind.geeknews" ./chrome --no-open --force`,
-  {
-    cwd: './dist/'
-  });
 
-  await exec(`xcodebuild -scheme geeknews-web-clipper SYMROOT="./dist" build`, {
+  const options = `--macos-only --app-name "geeknews-web-clipper" --project-location "./dist" --bundle-identifier "me.ragingwind.geeknews"`;
+  const addtional = `--no-open --force`;
+
+  await exec(`xcrun safari-web-extension-converter ${options} ./dist/extension ${addtional}`);
+  await exec(`xcodebuild -scheme geeknews-web-clipper -configuration Release SYMROOT="../safari" build`, {
     cwd: './dist/geeknews-web-clipper/'
   });
 }
 
 await clean();
+await build();
+
 await buildChrome();
+await buildFirefox();
 await buildSafari();
